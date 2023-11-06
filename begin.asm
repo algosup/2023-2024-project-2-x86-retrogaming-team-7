f5ebd45a5a73fe86ea905d869b2cfdb2f72aa8d6
HiNett <elone.delille@algosup.com>
2023-11-06 17:36:51 +0100

    Create begin.asm
    Test #1

---
+++ begin.asm
@@ -0,0 +1,78 @@
+section .data
+    score dw 0        ; Define score as a word (16 bits)
+    lives db 3        ; Define lives as a byte (8 bits)
+    user_choice db 0  ; Variable to store the user's choice
+
+menu_message db 'Main Menu', 0x0D, 0x0A
+            db '1. Start', 0x0D, 0x0A
+            db '2. Select Mode', 0x0D, 0x0A
+            db '3. Options', 0x0D, 0x0A
+            db 'Enter your choice: $'
+
+section .bss
+    ; Reserve space for dynamic parts of the maze or other variables
+
+section .text
+    global _start
+
+_start:
+    ; Set up the segment registers
+    mov ax, 0x07C0
+    add ax, 288
+    mov ss, ax
+    mov sp, 4096
+
+    ; Display the main menu
+    call display_menu
+    ; Get the user's choice
+    call get_user_choice
+
+    ; Based on the user's choice, take appropriate action
+    cmp byte [user_choice], '1'
+    je start_game
+    cmp byte [user_choice], '2'
+    je select_mode
+    cmp byte [user_choice], '3'
+    je options
+    jmp _start ; Invalid choice, display menu again
+
+start_game:
+    ; Set up the game
+    ; call initialize_game
+    ; ... rest of the game loop
+
+select_mode:
+    ; Implement mode selection logic here
+    ; This is where the user can choose game modes
+
+options:
+    ; Implement options menu logic here
+    ; This is where the user can configure game settings
+
+display_menu:
+    ; Set video mode to 13h (320x200 with 256 colors)
+    mov ah, 0x00       ; Function: Set Video Mode
+    mov al, 0x13       ; Mode: 320x200 256-color mode
+    int 0x10           ; BIOS video interrupt
+
+    ; Display the main menu on the screen
+    mov ah, 0x09         ; Function: Print String
+    mov dx, menu_message ; DX points to the menu message
+    int 0x21             ; DOS interrupt for displaying text
+    ret
+
+get_user_choice:
+    ; Read the user's choice from keyboard input
+    ; You can use BIOS interrupt for keyboard input
+    ; Example:
+    mov ah, 0x00         ; Function: Wait for Keypress
+    int 0x16             ; BIOS interrupt for keyboard input
+    mov ah, 0x01         ; Function: Read Keypress
+    int 0x16             ; BIOS interrupt for keyboard input
+    mov [user_choice], al ; Store the user's choice
+    ret
+
+; initialize_game:
+    ; Initialize game variables (score, lives, etc.)
+    ; Draw the static parts of the maze
+    ; ret
