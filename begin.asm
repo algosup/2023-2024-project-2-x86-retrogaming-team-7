3f8114aa07913df93cfec90db50ece0c7eb89640
DELILLE Elone <elone.delille@algosup.com>
2023-11-10 15:11:42 +0100

    Update begin.asm
    Updating code and adding touch control, to access option and category

--- begin.asm
+++ begin.asm
@@ -1,87 +1,128 @@
-section .data
-    mainMenuText db 'Pac-Man Game', 0Ah, '$'
-    startText db '1. Start Game', 0Ah, '$'
-    optionsText db '2. Options', 0Ah, '$'
-    exitText db '3. Exit', 0Ah, '$'
-    userInput db 0
+org 100h
 
-section .bss
-    player_score resb 10
-    player_lives resb 1
+section .data
+    welcomeMessage db 'Welcome to Pac-Man!', 0Dh, 0Ah, '$'
+    startGame db 'S - Start', 0Dh, 0Ah, '$'
+    gamemodeMessage db 'G - Gamemode', 0Dh, 0Ah, '$'
+    optionMessage db 'O - Option', 0Dh, 0Ah, '$'
+    exitGame db 'Echap - Exit', 0Dh, 0Ah, '$'
+    levelMessage db 'Level 1', 0Dh, 0Ah, '$'
+    CapsLockMessage db '/!\ You must turn ON your Caps Lock', 0Dh, 0Ah, '$'
 
 section .text
     global _start
 
-setup_environment:
-    mov ax, 0x0003  ; Set to text mode
+_start:
+    ; Set video mode to 80x25 color text (mode 03h)
+    mov ax, 0x0003
+    int 0x10
+    ; Print the welcome message
+    mov ah, 0x0E
+    mov si, welcomeMessage
+    call print_string
+    mov si, startGame
+    call print_string
+    mov si, gamemodeMessage
+    call print_string
+    mov si, optionMessage
+    call print_string
+    mov si, exitGame
+    call print_string
+    mov si, CapsLockMessage
+    call print_string
+
+    ; Wait for key press
+    mov ah, 0x00
+    int 0x16  ; BIOS keyboard service
+
+    ; Check if 'b' was pressed
+    cmp al, 'b'
+    je debug_option
+    cmp al, 0x1B
+    je exit_program
+    cmp al, '1'
+    je start_game
+    cmp al, '2'
+    je select_game_mode
+    cmp al, '3'
+    je option_setting
+
+hang:
+    jmp hang
+
+debug_option:
+    ; Clear the screen
+    call clear_screen
+    mov ax, 0x0003
+    int 0x10
+
+    ; Print level message
+    mov ah, 0x0E
+    mov si, levelMessage
+    call print_string
+
+    jmp hang  ; Hang after displaying level message
+
+start_game:
+    call clear_screen
+    mov ax, 0x0003
+    int 0x10
+    mov ah, 0x0E
+    mov si, startGame
+    call print_string
+    ; Wait for key press
+    mov ah, 0x00
+    int 0x16  ; BIOS keyboard service
+    cmp al, 0x1B
+    je _start
+
+select_game_mode:
+    call clear_screen
+    mov ax, 0x0003
+    int 0x10
+    mov ah, 0x0E
+    mov si, gamemodeMessage
+    call print_string
+    ; Wait for key press
+    mov ah, 0x00
+    int 0x16  ; BIOS keyboard service
+    cmp al, 0x1B
+    je _start
+
+option_setting:
+    call clear_screen
+    mov ax, 0x0003
+    int 0x10
+    mov ah, 0x0E
+    mov si, optionMessage
+    call print_string
+    ; Wait for key press
+    mov ah, 0x00
+    int 0x16  ; BIOS keyboard service
+    cmp al, 0x1B
+    je _start
+
+print_string:
+    next_char:
+        lodsb
+        cmp al, '$'
+        je end_string
+        int 0x10
+        jmp next_char
+    end_string:
+        ret
+
+clear_screen:
+    mov ah, 0x00
+    mov al, 0
+    mov bh, 0x07
+    mov cx, 0
+    mov dx, 184Fh
     int 0x10
     ret
 
-initialize_memory:
-    mov ax, 0
-    mov ds, ax
-    mov es, ax
-    ret
-
-main_menu:
-    mov ah, 0x0E    ; Teletype output function
-    mov si, mainMenuText
-    call print_string
-
-    mov si, startText
-    call print_string
-
-    mov si, optionsText
-    call print_string
-
-    mov si, exitText
-    call print_string
-
-menu_loop:
-    call wait_for_input
-
-    cmp byte [userInput], '1'
-    je start_game
-    cmp byte [userInput], '2'
-    je show_options
-    cmp byte [userInput], '3'
-    je exit_program
-    jmp menu_loop   ; Loop back if input is invalid
-
-    ret
-
-wait_for_input:
-    mov ah, 0x00
-    int 0x16        ; Wait for keypress
-    mov [userInput], al
-    ret
-
-print_string:
-    next_character:
-        lodsb
-        cmp al, '$'
-        je done_printing
-        int 0x10
-        jmp next_character
-    done_printing:
-        ret
-
-start_game:
-    ; Placeholder for starting the game
-    jmp exit_program ; Temporarily exit after this placeholder
-
-show_options:
-    ; Placeholder for showing options
-    jmp exit_program ; Temporarily exit after this placeholder
-
+; Function to exit the program and return to DOS
 exit_program:
-    mov ax, 0x4C00  ; Exit program
-    int 0x21
-    ret
-
-_start:
-    call setup_environment
-    call initialize_memory
-    call main_menu
-
-    jmp exit_program ; Ensure the program exits properly
+    mov ax, 0x4C00  ; Function 4Ch of interrupt 21h: Exit with return code
+    int 0x21        ; Call DOS interrupt
+    ret             ; The ret instruction here is for safety, but it should never be reached
