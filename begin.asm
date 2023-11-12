d3633635192fc32c55299b4f86e0909b75c4652d
DELILLE Elone <elone.delille@algosup.com>
2023-11-12 21:36:56 +0100

    Update begin.asm
    Addition of a menu of options and an analysis system to separate keys from their value. Real-time display of configuration menu keys via settings.txt file

--- begin.asm
+++ begin.asm
@@ -2,12 +2,20 @@ org 100h
 
 section .data
     welcomeMessage db 'Welcome to Pac-Man!', 0Dh, 0Ah, '$'
-    startGame db 'S - Start', 0Dh, 0Ah, '$'
-    gamemodeMessage db 'G - Gamemode', 0Dh, 0Ah, '$'
-    optionMessage db 'O - Option', 0Dh, 0Ah, '$'
+    startGame db '1 - Start', 0Dh, 0Ah, '$'
+    gamemodeMessage db '2 - Gamemode', 0Dh, 0Ah, '$'
+    optionMessage db '3 - Option', 0Dh, 0Ah, '$'
     exitGame db 'Echap - Exit', 0Dh, 0Ah, '$'
-    levelMessage db 'Level 1', 0Dh, 0Ah, '$'
+    levelMessage db 'Debugging Panel', 0Dh, 0Ah, '$'
     CapsLockMessage db '/!\ You must turn ON your Caps Lock', 0Dh, 0Ah, '$'
+
+    filename db 'SETTINGS.TXT', 0 ; DOS file names are uppercase
+    newLine db 13, 10, '$'  ; Carriage return, line feed, and DOS string terminator
+
+section .bss
+    fileHandle resw 1 ; Reserve word for file handle
+    buffer resb 1024 ; Reserve 1024 bytes for buffer
+    readBytes resw 1 ; Reserve word for number of bytes read
 
 section .text
     global _start
@@ -83,12 +91,62 @@ select_game_mode:
 
 option_setting:
     call clear_screen
+    ; Open the file
     mov ax, 0x0003
     int 0x10
-    mov ah, 0x0E
-    mov si, optionMessage
-    call print_string
+    mov ah, 0x3D ; Function: Open existing file
+    mov al, 0    ; Access mode: Read only
+    lea dx, filename
+    int 0x21
+    jc error ; Jump if carry flag is set (error)
+    mov [fileHandle], ax
+
+    ; Read from the file
+    mov ah, 0x3F ; Function: Read from file or device
+    mov bx, [fileHandle]
+    lea dx, [buffer]
+    mov cx, 1024 ; Number of bytes to read
+    int 0x21
+    jc error ; Jump if carry flag is set (error)
+    mov [readBytes], ax
+
+    ; Check if file is empty
+    cmp ax, 0
+    je close_file
+
+    ; Process and display the buffer content
+    mov si, 0         ; Source index for buffer iteration
+    mov cx, [readBytes]  ; Number of bytes to process
+
+process_buffer:
+    dec cx           ; Decrement remaining byte count
+    jz finished_processing ; Jump if all bytes are processed
+
+    mov ah, 0x02     ; Function to display character
+    mov dl, [buffer + si] ; Load current character from buffer into DL
+    int 0x21         ; Call interrupt to display the character
+
+    cmp byte [buffer + si], '=' ; Check if current character is '='
+    jne increment_si   ; Jump if not '='
+
+    ; Output newline when '=' is found
+    lea dx, newLine
+    mov ah, 0x09     ; Function to display string
+    int 0x21         ; Call interrupt
+
+increment_si:
+    inc si           ; Move to the next character
+    jmp process_buffer
+
+finished_processing:
     call wait_for_escape
+
+close_file:
+    ; Close the file
+    mov ah, 0x3E
+    mov bx, [fileHandle]
+    int 0x21
+    jc error
 
 print_string:
     next_char:
@@ -108,6 +166,11 @@ clear_screen:
     mov dx, 184Fh
     int 0x10
     ret
+
+error:
+    ; Handle error (for now, just exit)
+    mov ax, 0x4C01
+    int 0x21
 
 wait_for_escape:
     ; Wait for key press in a loop until 'Escape' is pressed
