1d11b4eda5778e0b2a8b7bec60b8d6e411429df6
DELILLE Elone <elone.delille@algosup.com>
2023-11-17 14:37:39 +0100

    Update mainmenu.cmd

--- MainMenu/mainmenu.cmd
+++ MainMenu/mainmenu.cmd
@@ -1,9 +1,3 @@
 @echo off
-set "SCRIPT_DIR=%~dp0"
-set "ROOT_DIR=%SCRIPT_DIR%..\..\.."
-set "NASM=%ROOT_DIR%\nasm\nasm"
-set "BIN_DIR=%ROOT_DIR%\bin"
-
-"%NASM%" mainmenu.asm -f bin -o "%BIN_DIR%\menu.com"
-
+nasm mainmenu.asm -f bin -o menu.com
 pause
