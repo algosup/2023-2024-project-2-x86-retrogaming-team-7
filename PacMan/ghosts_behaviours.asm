3d78b9b04bf89f5df7c42d06db7a59cb4401b370
DELILLE Elone <elone.delille@algosup.com>
2023-12-21 14:48:09 +0100

    Fully Implemented Automatic Behavior
    Ghosts now use randomness to move around the Maze.

--- PacMan/ghosts_behaviours.asm
+++ PacMan/ghosts_behaviours.asm
@@ -8,44 +8,47 @@ blinkyInitialized db 0
 inkyInitialized db 0
 clydeInitialized db 0
 pinkyInitialized db 0
+randomSeed dw 34201
+
+update_prng:
+    ; Formule simple pour le PRNG : seed = (seed * 32719 + 3) % 32768
+    mov ax, [randomSeed]
+    imul ax, 32719
+    add ax, 3
+    and ax, 0x7FFF  ; Garde seulement les 15 bits inférieurs
+    mov [randomSeed], ax
+    ret
 
 blinky_switch_direction:
     cmp byte [blinkyInitialized], 0
     je .notInitialized
     mov al, [detectCollision]
     cmp al, 0
-    je .no_collision  ; S'il n'y a pas de collision, ne rien faire
+    je .no_collision
 
 .try_direction:
-    ; Utiliser l'interruption 0x1A pour obtenir un nombre aléatoire
-    mov ah, 0x00
-    int 0x1A          ; AX contient maintenant une valeur aléatoire
-    and ax, 0x0003    ; AX = AX modulo 4
+    call update_prng   ; Mettre à jour le PRNG et obtenir un nouveau nombre aléatoire dans AX
+    mov dx, ax         ; Utiliser DX pour le calcul de la direction
 
-    cmp  dx, 0
-    jbe .go_right
-    cmp  dx, 1
+    cmp dx, 8191
     jbe .go_up
-    cmp  dx, 2
+    cmp dx, 16383
     jbe .go_left
-    cmp  dx, 3
-    je .go_down
+    cmp dx, 24575
+    jbe .go_down
+    jmp .go_right
 
 .go_right:
     mov ax, 4Dh
-    mov byte [blinkyStep], 0
     jmp .set_direction
 .go_up:
     mov ax, 48h
-    mov byte [blinkyStep], 1
     jmp .set_direction
 .go_left:
     mov ax, 4Bh
-    mov byte [blinkyStep], 2
     jmp .set_direction
 .go_down:
     mov ax, 50h
-    mov byte [blinkyStep], 3
 
 .set_direction:
     mov [actualBDirection], ax
@@ -66,46 +69,41 @@ blinky_switch_direction:
 .no_collision:
     ret
 
+
 clyde_switch_direction:
     cmp byte [clydeInitialized], 0
     je .notInitialized
     mov al, [detectCollision]
     cmp al, 0
-    je .no_collision  ; S'il n'y a pas de collision, ne rien faire
+    je .no_collision
 
-    ; Gérer le changement de direction en fonction de l'étape actuelle
-    cmp byte [clydeStep], 0
-    je .go_up
-    cmp byte [clydeStep], 1
-    je .go_down
-    cmp byte [clydeStep], 2
-    je .go_right
-    cmp byte [clydeStep], 3
-    je .go_left
+.try_direction:
+    call update_prng   ; Mettre à jour le PRNG et obtenir un nouveau nombre aléatoire dans AX
+    mov dx, ax         ; Utiliser DX pour le calcul de la direction
+
+    cmp dx, 8191
+    jbe .go_up
+    cmp dx, 16383
+    jbe .go_left
+    cmp dx, 24575
+    jbe .go_down
+    jmp .go_right
 
 .go_right:
     mov ax, 4Dh
-    mov byte [clydeStep], 1
     jmp .set_direction
 .go_up:
     mov ax, 48h
-    mov byte [clydeStep], 2
     jmp .set_direction
 .go_left:
     mov ax, 4Bh
-    mov byte [clydeStep], 3
     jmp .set_direction
 .go_down:
     mov ax, 50h
-    mov byte [clydeStep], 4
 
 .set_direction:
     mov [actualCDirection], ax
     call continue_movementC
-    ; Incrémenter blinkyStep et le réinitialiser si nécessaire
-    cmp byte [clydeStep], 4
-    jne .end
-    mov byte [clydeStep], 0
     jmp .end
 
 .notInitialized:
@@ -127,41 +125,35 @@ inky_switch_direction:
     je .notInitialized
     mov al, [detectCollision]
     cmp al, 0
-    je .no_collision  ; S'il n'y a pas de collision, ne rien faire
+    je .no_collision
 
-    ; Gérer le changement de direction en fonction de l'étape actuelle
-    cmp byte [inkyStep], 0
-    je .go_right
-    cmp byte [inkyStep], 1
-    je .go_up
-    cmp byte [inkyStep], 2
-    je .go_left
-    cmp byte [inkyStep], 3
-    je .go_down
+.try_direction:
+    call update_prng   ; Mettre à jour le PRNG et obtenir un nouveau nombre aléatoire dans AX
+    mov dx, ax         ; Utiliser DX pour le calcul de la direction
+
+    cmp dx, 8191
+    jbe .go_up
+    cmp dx, 16383
+    jbe .go_left
+    cmp dx, 24575
+    jbe .go_down
+    jmp .go_right
 
 .go_right:
     mov ax, 4Dh
-    mov byte [inkyStep], 1
     jmp .set_direction
 .go_up:
     mov ax, 48h
-    mov byte [inkyStep], 2
     jmp .set_direction
 .go_left:
     mov ax, 4Bh
-    mov byte [inkyStep], 3
     jmp .set_direction
 .go_down:
     mov ax, 50h
-    mov byte [inkyStep], 4
 
 .set_direction:
     mov [actualIDirection], ax
     call continue_movementB
-    ; Incrémenter blinkyStep et le réinitialiser si nécessaire
-    cmp byte [inkyStep], 4
-    jne .end
-    mov byte [inkyStep], 0
     jmp .end
 
 .notInitialized:
@@ -171,7 +163,6 @@ inky_switch_direction:
     call continue_movementI
 
 .end:
-    ; Réinitialiser l'indicateur de collision après avoir géré le changement de direction
     mov byte [detectCollision], 0
     ret
 
@@ -183,17 +174,19 @@ pinky_switch_direction:
     je .notInitialized
     mov al, [detectCollision]
     cmp al, 0
-    je .no_collision  ; S'il n'y a pas de collision, ne rien faire
+    je .no_collision
 
-    ; Gérer le changement de direction en fonction de l'étape actuelle
-    cmp byte [pinkyStep], 0
-    je .go_right
-    cmp byte [pinkyStep], 1
-    je .go_up
-    cmp byte [pinkyStep], 2
-    je .go_left
-    cmp byte [pinkyStep], 3
-    je .go_down
+.try_direction:
+    call update_prng   ; Mettre à jour le PRNG et obtenir un nouveau nombre aléatoire dans AX
+    mov dx, ax         ; Utiliser DX pour le calcul de la direction
+
+    cmp dx, 8191
+    jbe .go_up
+    cmp dx, 16383
+    jbe .go_left
+    cmp dx, 24575
+    jbe .go_down
+    jmp .go_right
 
 .go_right:
     mov ax, 4Dh
@@ -222,7 +215,7 @@ pinky_switch_direction:
 
 .notInitialized:
     mov byte [pinkyInitialized], 1
-    mov ax, 4Dh
+    mov ax, 50h
     mov [actualPDirection], ax
     call continue_movementP
 
